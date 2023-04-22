6cfdec7927666d17deb98ffb2deed1a970f21ede
psina <mr.samoylove@gmail.com>
2023-04-22 16:16:24 +0300

    sem4

---
+++ Sem5/task1.py
@@ -0,0 +1,12 @@
+# Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
+# Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+import os.path
+
+
+def path_split(fullpath: str) -> tuple:
+    path, fullname = fullpath.rsplit(os.path.sep, 1)
+    name, extension = fullname.split('.')
+    return path, name, extension
+
+
+print(path_split(r"C:\Users\mrsam\Desktop\Other\Резюме.pdf"))
---
+++ Sem5/task3.py
@@ -0,0 +1,15 @@
+# Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+
+
+def fib_generator():
+    a = 0
+    b = 1
+    while True:
+        yield b
+        a, b = b, a + b
+
+
+fib_seq = fib_generator()
+
+for _ in range(10):
+    print(next(fib_seq))
