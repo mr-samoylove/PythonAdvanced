4703c03fc49217ffcfb24f52d37b79057654425d
psina <mr.samoylove@gmail.com>
2023-04-28 13:46:53 +0300

    sem7_upd

--- Sem7/working_with_files/renaming.py
+++ Sem7/working_with_files/renaming.py
@@ -12,7 +12,6 @@ def rename_group_files(new_filename: str, target_extension: str, new_extension: 
     keep_to = range_of_name_to_keep[1]
     for name in os.listdir(os.getcwd()):
         if name.endswith(target_extension):
-            print(name)
             while os.path.isfile(new_filename + str(files_counter).zfill(width_of_id) + new_extension):
                 files_counter += 1
             os.rename(name,
